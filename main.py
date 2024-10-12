9b619a218656b1aea7ef79396dcd5ec78d1425b5
roudasaffuri <saffuri87@gmail.com>
2024-10-12 13:06:39 +0300

    Merge remote-tracking branch 'origin/master'

--- main.py
+++ main.py
@@ -9,10 +9,14 @@ from login import log
 from winesForSale import wineForSale
 from getProductByID import get_wine_by_id
 from updateProduct import updateProduct
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
+KEY = os.getenv("KEY")
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key_I_am_the_big_BOSS'  # Replace with a strong secret key for production
+app.secret_key = KEY  # Replace with a strong secret key for production
 
 @app.context_processor
 def inject_current_year():
