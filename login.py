48fa38adda8b1cd39778a60039f7541ae4c9229f
roudasaffuri <saffuri87@gmail.com>
2025-04-30 13:03:47 +0300

    payment2

--- login.py
+++ login.py
@@ -1,4 +1,4 @@
-import time
+
 from db_connection import create_connection, disconnection
 from flask import redirect, url_for, flash, session, request, render_template
 from fernet_encryption import decode_string
@@ -19,7 +19,7 @@ def log():
             result = cur.fetchone()
 
             if result is not None:
-                print(result)
+
                 id, stored_password_base64, is_admin, firstname, lastname = result
                 # Decode the stored password from base64
                 stored_password = base64.b64decode(stored_password_base64)
@@ -28,7 +28,7 @@ def log():
                 # Verify the password
                 if decrypted_password == password:
                     session['id'] = id
-                    print(session['id'])
+
                     # Check admin status
                     if useremail == 'admin@email.com' and is_admin:
                         session['admin'] = useremail
