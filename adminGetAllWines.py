36e43d1693341cc210bb21af7bf258d7c510dd16
roudasaffuri <saffuri87@gmail.com>
2025-07-15 13:28:04 +0300

    search products

--- adminGetAllWines.py
+++ adminGetAllWines.py
@@ -1,40 +1,66 @@
 from decimal import Decimal, ROUND_HALF_UP
+from flask import request
 from ClassWine import Wine
 from db_connection import create_connection, disconnection
 
+def calculate_discounted_price(wine):
+    price_dec = Decimal(wine.price)
+    discount_dec = Decimal(wine.discount)
+    hundred = Decimal('100')
+    new_price = ((hundred - discount_dec) / hundred) * price_dec
+    return new_price.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
+
 def getAllWines():
+    search = request.args.get('search', '').strip()
+    sort_by_stock = request.args.get('sort_by_stock')  # will be "1" if checkbox is checked
+
     conn = create_connection()
     cursor = conn.cursor()
 
     try:
-        cursor.execute("SELECT * FROM public.wines ORDER BY id ASC")
-        result = cursor.fetchall()  # Fetch all rows
         wines = []
+        base_query = "SELECT * FROM wines"
+        conditions = []
+        params = []
+
+        if search:
+            conditions.append("""
+                (
+                    CAST(id AS TEXT) ILIKE %s OR
+                    wine_name ILIKE %s OR
+                    wine_type ILIKE %s
+                )
+            """)
+            like_pattern = f"%{search}%"
+            params.extend([like_pattern, like_pattern, like_pattern])
+
+        # Add WHERE clause if conditions exist
+        if conditions:
+            base_query += " WHERE " + " AND ".join(conditions)
+
+        # Add ORDER BY
+        if sort_by_stock:
+            base_query += " ORDER BY stock ASC"
+        else:
+            base_query += " ORDER BY id ASC"
+
+        cursor.execute(base_query, tuple(params))
+        result = cursor.fetchall()
 
         for row in result:
-
-
             wine = Wine(*row)
 
-            if int(row[9]) == 0:
-                wines.append(wine)
-            else:
-                price_dec = Decimal(wine.price)
-                discount_dec = Decimal(wine.discount)
-                hundred = Decimal('100')
+            # row[9] = discount
+            if int(row[9]) != 0:
+                wine.price = calculate_discounted_price(wine)
 
-                new_price = ((hundred - discount_dec) / hundred) * price_dec
-                new_price = new_price.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
-
-                wine.price = new_price
-
-                wines.append(wine)
+            wines.append(wine)
 
         return wines
+
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
+
     finally:
         disconnection(conn, cursor)
-
-
