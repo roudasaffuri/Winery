f858478b8523aa404f25070639cc0b4eab465c42
roudasaffuri <saffuri87@gmail.com>
2025-09-06 15:09:22 +0300

    best_seller

--- userGetSinglePage.py
+++ userGetSinglePage.py
@@ -4,6 +4,7 @@ from userGetWineById import getWineById
 
 
 def userGetSinglePage(id):
+    # best_seller values  1=Bestseller 2=Recommended 3=Favorite
     best_seller = request.args.get('best_seller', default=0, type=int)
     wine = getWineById(id)
     return render_template("userSinglePage.html", wine=wine, best_seller=best_seller)
