68521399883c536846a7673b785f7635273da74c
roudasaffuri <saffuri87@gmail.com>
2025-09-06 12:28:42 +0300

    userAgeVerified

--- userAgeVerified.py
+++ userAgeVerified.py
@@ -1,14 +1,13 @@
 from flask import request, redirect, url_for, render_template
 
-
-def ageVerified():
-    age_verified = request.form['age_verified']
-    if age_verified == 'yes':
-        return redirect(url_for('login'))  # שולח לדף ה-login
-    else:
-        return redirect("https://www.google.com")  # מפנה לגוגל אם המשתמש לא עבר את האימות
-
 def userAgeVerified():
     if request.method == 'POST':
-        return ageVerified()
+        age_verified = request.form['age_verified']
+        if age_verified == 'yes':
+            return redirect(url_for('login'))  # שולח לדף ה-login
+        else:
+            return redirect("https://www.google.com")  # מפנה לגוגל אם המשתמש לא עבר את האימות
     return render_template("index.html")
+
+
+
